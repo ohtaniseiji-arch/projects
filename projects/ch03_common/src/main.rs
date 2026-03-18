edfc3d05f5272f4c24a45048429b1addb1912649
MESW ohtani <Ootani.Seiji.bx@mesw.co.jp>
2026-03-18 10:13:29 +0900

    book(ch03): common (homepage original)

--- projects/ch03_common/src/main.rs
+++ projects/ch03_common/src/main.rs
@@ -2,28 +2,22 @@ fn main() {
     // addition
     // 足し算
     let sum = 5 + 10;
-    println!("sum = {sum}");
 
     // subtraction
     // 引き算
     let difference = 95.5 - 4.3;
-    println!("difference = {difference}");
 
     // multiplication
     // 掛け算
     let product = 4 * 30;
-    println!("product = {product}");
 
     // division
     // 割り算
     let quotient = 56.7 / 32.2;
-    println!("quotient = {quotient}");
     let truncated = -5 / 3; // Results in -1
-    println!("truncated = {truncated}");
     // 結果は-1
 
     // remainder
     // 余り
     let remainder = 43 % 5;
-    println!("remainder = {remainder}");
 }

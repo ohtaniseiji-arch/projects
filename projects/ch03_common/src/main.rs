c50fd40973b2caa7181730bad39381ef3199a104
MESW ohtani <Ootani.Seiji.bx@mesw.co.jp>
2026-03-19 08:16:03 +0000

    book(ch03): char

--- projects/ch03_common/src/main.rs
+++ projects/ch03_common/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    let t = true;
-
-    let f: bool = false; // with explicit type annotation
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
     // 明示的型注釈付きで
+    let heart_eyed_cat = '😻'; //ハート目の猫
+    println!("{}", heart_eyed_cat); // ← ここにブレークポイントを置く
 }

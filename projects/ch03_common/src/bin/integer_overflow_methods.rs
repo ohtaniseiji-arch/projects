f3949b133d8c24169b80ebe42ee8934138964db3
MESW ohtani <Ootani.Seiji.bx@mesw.co.jp>
2026-03-19 04:33:45 +0000

    Add integer overflow method samples for chapter 3

---
+++ projects/ch03_common/src/bin/integer_overflow_methods.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let a: u8 = 255;
+    let b: u8 = 1;
+
+    // checked_*: オーバーフロー時は None
+    let checked = a.checked_add(b);
+    println!("checked_add: {:?}", checked);
+
+    // overflowing_*: (結果, オーバーフローしたか)
+    let (value, overflowed) = a.overflowing_add(b);
+    println!("overflowing_add: value={value}, overflowed={overflowed}");
+
+    // wrapping_*: ラップアラウンド
+    let wrapped = a.wrapping_add(b);
+    println!("wrapping_add: {wrapped}");
+
+    // saturating_*: 最小値/最大値で飽和
+    let saturated = a.saturating_add(b);
+    println!("saturating_add: {saturated}");
+
+    let x: u8 = 0;
+    println!("checked_sub: {:?}", x.checked_sub(1));
+    println!("overflowing_sub: {:?}", x.overflowing_sub(1));
+    println!("wrapping_sub: {}", x.wrapping_sub(1));
+    println!("saturating_sub: {}", x.saturating_sub(1));
+}

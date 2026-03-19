ce9ee228330746bf2988e976f1cb7fc3069e6040
MESW ohtani <Ootani.Seiji.bx@mesw.co.jp>
2026-03-19 07:08:42 +0000

    book(ch03): bool

--- projects/ch03_common/src/main.rs
+++ projects/ch03_common/src/main.rs
@@ -1,23 +1,6 @@
 fn main() {
-    // addition
-    // 足し算
-    let sum = 5 + 10;
+    let t = true;
 
-    // subtraction
-    // 引き算
-    let difference = 95.5 - 4.3;
-
-    // multiplication
-    // 掛け算
-    let product = 4 * 30;
-
-    // division
-    // 割り算
-    let quotient = 56.7 / 32.2;
-    let truncated = -5 / 3; // Results in -1
-    // 結果は-1
-
-    // remainder
-    // 余り
-    let remainder = 43 % 5;
+    let f: bool = false; // with explicit type annotation
+    // 明示的型注釈付きで
 }
